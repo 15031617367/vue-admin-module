30109554e3751e851bea89064c892c1293dc7583
李承斌 <1307437149@qq.com>
2019-07-30 21:31:07 +0800

    柱状图模块化练习测试

--- src/components/bars.vue
+++ src/components/bars.vue
@@ -1,27 +1,43 @@
 <template>
     <div class="">
-        雷达图
+        <h1>模块化echarts图表</h1>
+        <div>
+            <el-button type="primary" @click="btnsBar(0)">一月</el-button>
+            <el-button type="success" @click="btnsBar(1)">二月</el-button>
+            <el-button type="info" @click="btnsBar(2)">三月</el-button>
+        </div>
+        <div class="charts" id="chartsBar"></div>
     </div>
 </template>
-
 <script>
+import test from "../module/echarts/bar"
 export default {
     data() { 
         return {
-
+            month:['../../static/json/bar/1month.json','../../static/json/bar/2month.json','../../static/json/bar/3month.json']
         }
     },
+    mounted(){
+    //    test.test('chartsBar')
+    this.btnsBar(0)
+    },
 
-    mounted(){},
-
-    methods:{},
+    methods:{
+        btnsBar(index){
+            this.$axios({
+                methods:'get',
+                url:this.month[index]
+            }).then((response)=>{
+                test.test(response.data.data,'chartsBar')
+            })
+        }
+    },
 }
 </script>
 
 <style scoped>
-
-
-
-
-
+.charts{
+    width: 100%;
+    height: 500px;
+}
 </style>
--- src/module/echarts/bar.js
+++ src/module/echarts/bar.js
@@ -3,5 +3,32 @@ var echarts = require("echarts/lib/echarts");
     require("echarts/lib/chart/radar");
     require("echarts/lib/component/tooltip");
     require("echarts/lib/component/title");
-
+let test = function(data,id){
+    let option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar'
+        }]
+    };
+    let xAxis = [];
+    let series = [];
+    for(let i=0;i<data.length;i++){
+        xAxis[i] = data[i].name;
+        series[i] = data[i].avg;
+    }
+    option.xAxis.data = xAxis;
+    option.series[0].data = series;
+    let mychart = echarts.init(document.getElementById(id));
+    mychart.setOption(option);
+}
+export default {
+    test
+}
 
